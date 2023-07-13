e5f4fcc89835c5eb64d346b148ef0a76b45305aa
HubertAndrzej <hubertandrzejsobolewski@gmail.com>
2023-07-13 22:47:49 +0200

    Added themes (dark and light mode)

---
+++ lib/providers/dark_theme_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/services/dark_theme_preferences.dart';
+
+class DarkThemeProvider with ChangeNotifier {
+  DarkThemePreferences darkThemePreferences = DarkThemePreferences();
+  bool _darkTheme = false;
+  bool get getDarkTheme => _darkTheme;
+  set setDarkTheme(bool value) {
+    _darkTheme = value;
+    darkThemePreferences.setDarkTheme(value);
+    notifyListeners();
+  }
+}
