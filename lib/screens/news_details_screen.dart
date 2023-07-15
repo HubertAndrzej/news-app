cb381010d1c6aca32e42e7a415619dc103be2a48
HubertAndrzej <hubertandrzejsobolewski@gmail.com>
2023-07-15 02:38:23 +0200

    Added modal bottom sheet

--- lib/screens/news_details_screen.dart
+++ lib/screens/news_details_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:news_app/services/utils.dart';
+import 'package:news_app/widgets/vertical_spacing_widget.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class NewsDetailsScreen extends StatefulWidget {
@@ -13,6 +18,8 @@ class NewsDetailsScreen extends StatefulWidget {
 class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
   late WebViewController _webViewController;
   double _progress = 0;
+  final _url =
+      'https://techcrunch.com/2022/06/17/marc-lores-food-delivery-startup-wonder-raises-350m-3-5b-valuation/';
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +50,7 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
           ),
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: () async => await _showModalBottomSheetFunction(),
               icon: const Icon(Icons.more_horiz),
             ),
           ],
@@ -57,8 +64,7 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
             ),
             Expanded(
               child: WebView(
-                initialUrl:
-                    'https://techcrunch.com/2022/06/17/marc-lores-food-delivery-startup-wonder-raises-350m-3-5b-valuation/',
+                initialUrl: _url,
                 zoomEnabled: true,
                 onProgress: (progress) {
                   setState(
@@ -68,6 +74,88 @@ class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
                 onWebViewCreated: (controller) =>
                     _webViewController = controller,
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showModalBottomSheetFunction() async {
+    await showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(20),
+        ),
+      ),
+      context: context,
+      builder: (context) => Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: const BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const VerticalSpacingWidget(height: 20),
+            Center(
+              child: Container(
+                height: 5,
+                width: 35,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            const VerticalSpacingWidget(height: 20),
+            const Text(
+              'More options',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 20,
+              ),
+            ),
+            const VerticalSpacingWidget(height: 20),
+            const Divider(thickness: 2),
+            const VerticalSpacingWidget(height: 20),
+            ListTile(
+              onTap: () async {
+                try {
+                  await Share.share(
+                    _url,
+                    subject: 'Look what I made!',
+                  );
+                } catch (error) {
+                  log('error occured $error' as num);
+                }
+              },
+              leading: const Icon(Icons.share),
+              title: const Text('Share'),
+            ),
+            ListTile(
+              onTap: () async {
+                if (!await launchUrl(Uri.parse(_url))) {
+                  throw Exception('Could not launch $_url');
+                }
+              },
+              leading: const Icon(Icons.open_in_browser),
+              title: const Text('Open in browser'),
+            ),
+            ListTile(
+              onTap: () async {
+                try {
+                  await _webViewController.reload();
+                } catch (error) {
+                  log('error occured $error' as num);
+                } finally {
+                  Navigator.pop(context);
+                }
+              },
+              leading: const Icon(Icons.refresh),
+              title: const Text('Refresh'),
             ),
           ],
         ),
